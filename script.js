767a622a3f113ea829d4bfa74e055a3804757f30
Kate Kramer <katie@kramerdesign.ca>
2020-06-22 14:25:09 -0400

    Adding local storage

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
-
+const filterOption = document.querySelector('.filter-todo')
 //event listeners
 todoButton.addEventListener('click', addTodo)
 todoList.addEventListener('click', deleteCheck)
+filterOption.addEventListener('click', filterTodo)
 
 //functions
 function addTodo(e) {
@@ -21,6 +22,8 @@ function addTodo(e) {
   newTodo.innerText = todoInput.value
   newTodo.classList.add('todo-item')
   todoDiv.appendChild(newTodo)
+  //add todo to local storage
+  saveLocalTodos(todoInput.value)
 
   //check button
   const completedButton = document.createElement('button')
@@ -51,4 +54,40 @@ function deleteCheck(e) {
     const todo = item.parentElement
     todo.classList.toggle('completed')
   }
+}
+function filterTodo(e) {
+  const todos = todoList.childNodes
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex'
+        break
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex'
+        } else {
+          todo.style.display = 'none'
+        }
+        break
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex'
+        } else {
+          todo.style.display = 'none'
+        }
+        break
+    }
+  })
+}
+
+function saveLocalTodos(todo) {
+  //check for existing storage
+  let todos
+  if (localStorage.getItem('todos') === null) {
+    todos = []
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+  todos.push(todo)
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
